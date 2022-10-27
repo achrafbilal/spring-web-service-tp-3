43c95b3581776416812e778058b71e6079dde546
Bilal Achraf <achraf.cdma@gmail.com>
2022-10-27 11:19:06 +0200

    Customer Service is enabled but not linked to gateway or eureka

---
+++ customer-service/src/main/java/com/bilalachraf/customerservice/entities/CustomerProjection.java
@@ -0,0 +1,2 @@
+package com.bilalachraf.customerservice.entities;public interface CustomerProjection {
+}
---
+++ customer-service/src/main/java/com/bilalachraf/customerservice/repositories/CustomerRepository.java
@@ -0,0 +1,2 @@
+package com.bilalachraf.customerservice.repositories;public interface CustomerRepository {
+}
