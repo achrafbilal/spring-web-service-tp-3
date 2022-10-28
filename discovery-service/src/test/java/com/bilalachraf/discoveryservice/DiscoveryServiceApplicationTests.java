b0e86d4532206fb05c12e32f3c469f4f36ce8bfa
achraf bilal <bilal_achraf@emsi-edu.ma>
2022-10-28 15:23:51 +0100

    discovery service is implemented using static RouteLocator in gateway service

---
+++ discovery-service/src/test/java/com/bilalachraf/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilalachraf.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- gateway-service/src/main/java/com/bilalachraf/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/bilalachraf/gatewayservice/GatewayServiceApplication.java
@@ -18,8 +18,8 @@ public class GatewayServiceApplication {
 	RouteLocator gatewayRoutes(RouteLocatorBuilder builder)
 	{
 		return builder.routes()
-				.route(r-> r.path("/customers/**").uri("http://localhost:8081/"))
-				.route(r-> r.path("/products/**").uri("http://localhost:8082/"))
+				.route(r-> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+				.route(r-> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
 				.build();
 	}
 
