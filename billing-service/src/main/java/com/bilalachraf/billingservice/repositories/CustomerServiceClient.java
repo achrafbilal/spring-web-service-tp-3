3b50a44341fda1ac99a427b86a19e3f6a35c9e55
Zohair diab <zohairdiab07@gmail.com>
2022-11-17 12:40:25 +0100

    Micro service name

--- billing-service/src/main/java/com/bilalachraf/billingservice/repositories/CustomerServiceClient.java
+++ billing-service/src/main/java/com/bilalachraf/billingservice/repositories/CustomerServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service")
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerServiceClient {
     @GetMapping("/customers/{id}?projection=fullCustomer")
     Customer findCustomerByID(@PathVariable("id") Long id);
